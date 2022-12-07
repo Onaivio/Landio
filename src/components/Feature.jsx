ef0840e2b595a52a6a02cbbd26c6ed3ec38e6055
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-07 22:58:31 +0100

    ..

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Feature = () => {
   return (
     <>
-      
       <section className="features" id="features">
         <div className="container">
-
           <h2 className="h2 section-title">Awesome Features</h2>
 
           <p className="section-text">
-            Et harum quidem rerum facilis est et expedita distinctio nam libero tempore cum soluta nobis eligendi
-            cumque.
+            Et harum quidem rerum facilis est et expedita distinctio nam libero
+            tempore cum soluta nobis eligendi cumque.
           </p>
 
           <div className="features-wrapper">
-
             <figure className="features-banner">
               <img src="./features-img-1.png" alt="illustration art" />
             </figure>
 
             <div className="features-content">
-
               <p className="features-content-subtitle">
                 <ion-icon name="sparkles"></ion-icon>
 
@@ -29,18 +25,17 @@ const Feature = () => {
               </p>
 
               <h3 className="features-content-title">
-                Build <strong>community</strong> & <strong>conversion</strong> with our suite of <strong>social
-                  tool</strong>
+                Build <strong>community</strong> & <strong>conversion</strong>{" "}
+                with our suite of <strong>social tool</strong>
               </h3>
 
               <p className="features-content-text">
-                Temporibus autem quibusdam et aut officiis debitis aut rerum a necessitatibus saepe eveniet ut et
-                voluptates repudiandae
-                sint molestiae non recusandae itaque.
+                Temporibus autem quibusdam et aut officiis debitis aut rerum a
+                necessitatibus saepe eveniet ut et voluptates repudiandae sint
+                molestiae non recusandae itaque.
               </p>
 
               <ul className="features-list">
-
                 <li className="features-list-item">
                   <ion-icon name="layers-outline"></ion-icon>
 
@@ -52,7 +47,6 @@ const Feature = () => {
 
                   <p>Cras ultricies mi eu turpis hendrerit fringilla.</p>
                 </li>
-
               </ul>
 
               <div className="btn-group">
@@ -60,19 +54,15 @@ const Feature = () => {
 
                 <button className="btn btn-secondary">Buy Now</button>
               </div>
-
             </div>
-
           </div>
 
           <div className="features-wrapper">
-
             <figure className="features-banner">
               <img src="./features-img-2.png" alt="illustration art" />
             </figure>
 
             <div className="features-content">
-
               <p className="features-content-subtitle">
                 <ion-icon name="sparkles"></ion-icon>
 
@@ -80,17 +70,17 @@ const Feature = () => {
               </p>
 
               <h3 className="features-content-title">
-                We do the work you <strong>stay focused</strong> on <strong>your customers.</strong>
+                We do the work you <strong>stay focused</strong> on{" "}
+                <strong>your customers.</strong>
               </h3>
 
               <p className="features-content-text">
-                Temporibus autem quibusdam et aut officiis debitis aut rerum a necessitatibus saepe eveniet ut et
-                voluptates repudiandae
-                sint molestiae non recusandae itaque.
+                Temporibus autem quibusdam et aut officiis debitis aut rerum a
+                necessitatibus saepe eveniet ut et voluptates repudiandae sint
+                molestiae non recusandae itaque.
               </p>
 
               <ul className="features-list">
-
                 <li className="features-list-item">
                   <ion-icon name="rocket-outline"></ion-icon>
 
@@ -102,7 +92,6 @@ const Feature = () => {
 
                   <p>Cras ultricies mi eu turpis hendrerit fringilla.</p>
                 </li>
-
               </ul>
 
               <div className="btn-group">
@@ -110,16 +99,12 @@ const Feature = () => {
 
                 <button className="btn btn-secondary">Buy Now</button>
               </div>
-
             </div>
-
           </div>
-
         </div>
       </section>
-
     </>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
