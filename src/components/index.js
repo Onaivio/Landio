74c078ca2633feb7fc7fb81a48e7006153f85911
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-07 22:31:04 +0100

    ..

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as Navbar } from './Navbar';
 export { default as Hero } from './Hero';
-export { default as AboutUs } from './AboutUs'
+export { default as AboutUs } from './AboutUs';
+export { default as Blog } from './Blog';
+export { default as Feature } from './Feature';
+export { default as Contact } from './Contact';
+export { default as Footer } from './Footer';
