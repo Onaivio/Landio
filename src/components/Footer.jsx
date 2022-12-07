15a2cbd3096b3f3035874acc4e0821bd2354b40b
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-07 22:56:20 +0100

    ..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,143 +4,143 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div class="footer-top">
-          <div class="container">
-            <div class="footer-brand">
-              <a href="#" class="logo">
+        <div className="footer-top">
+          <div className="container">
+            <div className="footer-brand">
+              <a href="#" className="logo">
                 <img src="./logo-footer.svg" alt="Landio logo" />
               </a>
 
-              <p class="footer-text">
+              <p className="footer-text">
                 Cras ultricies mi eu turpis sit hendrerit fringilla vestibulum
                 ante ipsum primis in faucibus ultrices posuere cubilia.
               </p>
 
-              <ul class="social-list">
+              <ul className="social-list">
                 <li>
-                  <a href="https://www.facebook.com/profile.php?id=100071660391192" class="social-link">
+                  <a href="https://www.facebook.com/profile.php?id=100071660391192" className="social-link">
                     <ion-icon name="logo-facebook"></ion-icon>
                   </a>
                 </li>
 
                 <li>
-                  <a href="https://twitter.com/OkomayinOnaivi" class="social-link">
+                  <a href="https://twitter.com/OkomayinOnaivi" className="social-link">
                     <ion-icon name="logo-twitter"></ion-icon>
                   </a>
                 </li>
 
                 <li>
-                  <a href="https://www.instagram.com/isaaccodetech/" class="social-link">
+                  <a href="https://www.instagram.com/isaaccodetech/" className="social-link">
                     <ion-icon name="logo-instagram"></ion-icon>
                   </a>
                 </li>
 
                 <li>
-                  <a href="https://www.linkedin.com/in/onaivi-okomayin-732b82241/" class="social-link">
+                  <a href="https://www.linkedin.com/in/onaivi-okomayin-732b82241/" className="social-link">
                     <ion-icon name="logo-linkedin"></ion-icon>
                   </a>
                 </li>
               </ul>
             </div>
 
-            <div class="footer-link-box">
-              <ul class="footer-list">
+            <div className="footer-link-box">
+              <ul className="footer-list">
                 <li>
-                  <p class="footer-item-title">ABOUT US</p>
+                  <p className="footer-item-title">ABOUT US</p>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Works
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Strategy
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Releases
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Press
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Mission
                   </a>
                 </li>
               </ul>
 
-              <ul class="footer-list">
+              <ul className="footer-list">
                 <li>
-                  <p class="footer-item-title">CUSTOMERS</p>
+                  <p className="footer-item-title">CUSTOMERS</p>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Tranding
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Popular
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Customers
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Features
                   </a>
                 </li>
               </ul>
 
-              <ul class="footer-list">
+              <ul className="footer-list">
                 <li>
-                  <p class="footer-item-title">SUPPORT</p>
+                  <p className="footer-item-title">SUPPORT</p>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Developers
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Support
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Customer Service
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Get Started
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="footer-link">
+                  <a href="#" className="footer-link">
                     Guide
                   </a>
                 </li>
@@ -149,9 +149,9 @@ const Footer = () => {
           </div>
         </div>
 
-        <div class="footer-bottom">
-          <div class="container">
-            <p class="copyright">
+        <div className="footer-bottom">
+          <div className="container">
+            <p className="copyright">
               &copy; 2022 <a href="onaivi-portfolio.vercel.app">onaivio.jsx</a>. All Right Reserved
             </p>
           </div>
