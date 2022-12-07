42f92339a182c0d5c950b1bafe5fd9ca231d615f
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-07 22:59:00 +0100

    ..

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Blog = () => {
   return (
     <div>
-            <section className="blog" id="blog">
+      <section className="blog" id="blog">
         <div className="container">
-
           <h2 className="h2 section-title">Latest News</h2>
 
           <p className="section-text">
-            Et harum quidem rerum facilis est et expedita distinctio nam libero tempore cum soluta nobis eligendi
-            cumque.
+            Et harum quidem rerum facilis est et expedita distinctio nam libero
+            tempore cum soluta nobis eligendi cumque.
           </p>
 
           <ul className="blog-list">
-
             <li>
               <div className="blog-card">
-
                 <figure className="blog-banner">
                   <img src="./blog-banner-1.jpg" alt="Best Traveling Place" />
                 </figure>
 
                 <div className="blog-meta">
-
                   <span>
                     <ion-icon name="calendar-outline"></ion-icon>
 
@@ -35,15 +31,13 @@ const Blog = () => {
 
                     <p>admin</p>
                   </span>
-
                 </div>
 
                 <h3 className="blog-title">Best Traveling Place</h3>
 
                 <p className="blog-text">
-                  Integer ante arcu accumsan a consectetuer eget posuere mauris praesent adipiscing phasellus
-                  ullamcorper ipsum rutrum
-                  punc.
+                  Integer ante arcu accumsan a consectetuer eget posuere mauris
+                  praesent adipiscing phasellus ullamcorper ipsum rutrum punc.
                 </p>
 
                 <a href="#" className="blog-link-btn">
@@ -51,19 +45,16 @@ const Blog = () => {
 
                   <ion-icon name="chevron-forward-outline"></ion-icon>
                 </a>
-
               </div>
             </li>
 
             <li>
               <div className="blog-card">
-
                 <figure className="blog-banner">
                   <img src="./blog-banner-2.jpg" alt="Private Meeting Room" />
                 </figure>
 
                 <div className="blog-meta">
-
                   <span>
                     <ion-icon name="calendar-outline"></ion-icon>
 
@@ -75,15 +66,13 @@ const Blog = () => {
 
                     <p>admin</p>
                   </span>
-
                 </div>
 
                 <h3 className="blog-title">Private Meeting Room</h3>
 
                 <p className="blog-text">
-                  Integer ante arcu accumsan a consectetuer eget posuere mauris praesent adipiscing phasellus
-                  ullamcorper ipsum rutrum
-                  punc.
+                  Integer ante arcu accumsan a consectetuer eget posuere mauris
+                  praesent adipiscing phasellus ullamcorper ipsum rutrum punc.
                 </p>
 
                 <a href="#" className="blog-link-btn">
@@ -91,19 +80,19 @@ const Blog = () => {
 
                   <ion-icon name="chevron-forward-outline"></ion-icon>
                 </a>
-
               </div>
             </li>
 
             <li>
               <div className="blog-card">
-
                 <figure className="blog-banner">
-                  <img src="./blog-banner-3.jpg" alt="The Best Business Ideas" />
+                  <img
+                    src="./blog-banner-3.jpg"
+                    alt="The Best Business Ideas"
+                  />
                 </figure>
 
                 <div className="blog-meta">
-
                   <span>
                     <ion-icon name="calendar-outline"></ion-icon>
 
@@ -115,15 +104,13 @@ const Blog = () => {
 
                     <p>admin</p>
                   </span>
-
                 </div>
 
                 <h3 className="blog-title">The Best Business Ideas</h3>
 
                 <p className="blog-text">
-                  Integer ante arcu accumsan a consectetuer eget posuere mauris praesent adipiscing phasellus
-                  ullamcorper ipsum rutrum
-                  punc.
+                  Integer ante arcu accumsan a consectetuer eget posuere mauris
+                  praesent adipiscing phasellus ullamcorper ipsum rutrum punc.
                 </p>
 
                 <a href="#" className="blog-link-btn">
@@ -131,16 +118,13 @@ const Blog = () => {
 
                   <ion-icon name="chevron-forward-outline"></ion-icon>
                 </a>
-
               </div>
             </li>
-
           </ul>
-
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
