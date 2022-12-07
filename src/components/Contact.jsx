a658641efdb18a0c6247683fc1e7c2b1374445ea
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-07 22:58:46 +0100

    ..

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,54 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <>
-     <section className="contact" id="contact">
+      <section className="contact" id="contact">
         <div className="container">
-
           <h2 className="h2 section-title">Contact Us</h2>
 
           <p className="section-text">
-            Et harum quidem rerum facilis est et expedita distinctio nam libero tempore cum soluta nobis eligendi
-            cumque.
+            Et harum quidem rerum facilis est et expedita distinctio nam libero
+            tempore cum soluta nobis eligendi cumque.
           </p>
 
           <div className="contact-wrapper">
-
             <form action="" className="contact-form">
-
               <div className="wrapper-flex">
-
                 <div className="input-wrapper">
                   <label for="name">Name*</label>
 
-                  <input type="text" name="name" id="name" required placeholder="Enter Your Name" className="input-field" />
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    required
+                    placeholder="Enter Your Name"
+                    className="input-field"
+                  />
                 </div>
 
                 <div className="input-wrapper">
                   <label for="emai">Email*</label>
 
-                  <input type="text" name="email" id="email" required placeholder="Enter Your Email"
-                    className="input-field" />
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    required
+                    placeholder="Enter Your Email"
+                    className="input-field"
+                  />
                 </div>
-
               </div>
 
               <label for="message">Message*</label>
 
-              <textarea name="message" id="message" required placeholder="Type Your Message"
-                className="input-field"></textarea>
+              <textarea
+                name="message"
+                id="message"
+                required
+                placeholder="Type Your Message"
+                className="input-field"
+              ></textarea>
 
               <button type="submit" className="btn btn-primary">
                 <span>Send Message</span>
 
                 <ion-icon name="paper-plane-outline"></ion-icon>
               </button>
-
             </form>
 
             <ul className="contact-list">
-
               <li>
                 <a href="mailto:onaivio220@gmail.com" className="contact-link">
                   <ion-icon name="mail-outline"></ion-icon>
@@ -88,15 +99,12 @@ const Contact = () => {
                   <address>: No. 64 Ahmadu Bello Way, Kano</address>
                 </a>
               </li>
-
             </ul>
-
           </div>
-
         </div>
-      </section> 
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
